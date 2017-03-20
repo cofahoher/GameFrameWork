8a9845feda77bcd9c9011dcf5cfbd1b8ce602935
cofahoher <cofahoher@163.com>
2017-03-20 10:55:46 +0800

    upload

---
+++ CLIENT/Assets/Scripts/GameGlobal.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGlobal : MonoBehaviour
+{
+    static GameGlobal ms_instance;
+    public static GameGlobal Instance
+    {
+        get { return ms_instance; }
+    }
+
+    void Awake()
+    {
+        ms_instance = this;
+        UnityEngine.Object.DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        InitSyncModelTest();
+    }
+
+    void Update()
+    {
+        UpdateSyncModelTest();
+    }
+
+    #region 测试同步模型
+    Combat.SyncTester m_sync_tester = null;
+    Combat.CombatTester m_combat_tester = null;
+
+    void InitSyncModelTest()
+    {
+        if (m_sync_tester != null)
+            return;
+        m_sync_tester = new Combat.SyncTester(this);
+        m_sync_tester.Init();
+        m_combat_tester = new Combat.CombatTester();
+        m_combat_tester.Init();
+    }
+
+    void UpdateSyncModelTest()
+    {
+        if (m_sync_tester != null)
+            m_sync_tester.Update();
+        if (m_combat_tester != null)
+            m_combat_tester.Update();
+    }
+
+    public delegate void LoadSceneCallback(int scene_build_index);
+    public LoadSceneCallback m_loadscene_callback = null;
+    void OnLevelWasLoaded(int scene_build_index)
+    {
+        if (m_loadscene_callback != null)
+            m_loadscene_callback(scene_build_index);
+    }
+    #endregion
+}
---
+++ CLIENT/Assets/Scripts/LogWrapper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogWrapper
+{
+    private static string ToStrBuff(object[] data)
+    {
+        StringBuilder sb = new StringBuilder(256);
+        for (int i = 0; i < data.Length; ++i)
+        {
+            sb.AppendFormat(" {0}", data[i]);
+        }
+        return sb.ToString();
+    }
+
+    public static void LogDebug(params object[] data)
+    {
+        LogDebug(ToStrBuff(data));
+    }
+
+    public static void LogDebug(string info)
+    {
+        LogDebug(info);
+    }
+
+    public static void LogInfo(params object[] data)
+    {
+        LogInfo(ToStrBuff(data));
+    }
+
+    public static void LogInfo(string info)
+    {
+        LogInfo(info);
+    }
+    public static void LogWarning(params object[] data)
+    {
+        LogWarning(ToStrBuff(data));
+    }
+
+    public static void LogWarning(string info)
+    {
+        LogWarning(info);
+    }
+
+    public static void LogError(params object[] data)
+    {
+        LogError(ToStrBuff(data));
+    }
+
+    public static void LogError(string info)
+    {
+        LogError(info);
+    }
+
+    public static void LogCritical(params object[] data)
+    {
+        LogError(ToStrBuff(data));
+    }
+
+    public static void LogCritical(string info)
+    {
+        LogError(info);
+    }
+}
