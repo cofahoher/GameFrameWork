18fead54649184ca06c3010a6ff2424f997f004f
cofahoher <cofahoher@163.com>
2017-05-11 22:39:45 +0800

    variable

--- CLIENT/Assets/Scripts/CombatModule/LogicWorld/ExpressionEngine/IExpressionVariableProvider.cs
+++ CLIENT/Assets/Scripts/CombatModule/LogicWorld/ExpressionEngine/IExpressionVariableProvider.cs
@@ -4,37 +4,21 @@ namespace Combat
 {
     public class ExpressionVariable : IRecyclable, IDestruct
     {
-        public static readonly int VID_Object = (int)CRC.Calculate("Object");           //最好别用，可能你说不清楚是下面四个中的哪个
-        public static readonly int VID_Player = (int)CRC.Calculate("Player");           //所属Player
-        public static readonly int VID_Entity = (int)CRC.Calculate("Entity");           //所属Entity
-        //public static readonly int VID_Skill = (int)CRC.Calculate("Skill");             //所属Skill
-        //public static readonly int VID_Effect = (int)CRC.Calculate("Effect");           //所属Effect
-        public static readonly int VID_LevelTable = (int)CRC.Calculate("LevelTable");   //等级数值表
-        public static readonly int VID_Attribute = (int)CRC.Calculate("Attribute");     //属性
-        public static readonly int VID_Value = (int)CRC.Calculate("Value");             //属性
-        public static readonly int VID_BaseValue = (int)CRC.Calculate("BaseValue");     //属性
-        public static readonly int VID_Source = (int)CRC.Calculate("Source");           //当前技能施放者
-        public static readonly int VID_Target = (int)CRC.Calculate("Target");           //当前技能目标
-        public static readonly int VID_Attacker = (int)CRC.Calculate("Attacker");       //当前攻击来源
-        public static readonly int VID_Defender = (int)CRC.Calculate("Defender");       //当前攻击目标
-        public static readonly int VID_BaseDamage = (int)CRC.Calculate("BaseDamage");   //当前攻击初始伤害
-        /*
-         * 在各种上下文（指IExpressionVariableProvider实现者），都可以这样写公式的一部分
-         * max_speed
-         * angle
-         * Object.angle
-         * Object.max_speed
-         * Entity.max_speed
-         * Attribute.Strength.Value
-         * Object.Attribute.Strength.Value
-         * 
-         * 在AttributeManagerComponent和Attribute上下文，还可以这么写
-         * Strength.Value
-         * 
-         * 在Attribute上下文，还可以这么写
-         * Value
-         * BaseValue
-         */
+        public static readonly int VID_Object = (int)CRC.Calculate("Object");                   //最好别用，可能你说不清楚是下面四个中的哪个
+        public static readonly int VID_Player = (int)CRC.Calculate("Player");                   //所属Player
+        public static readonly int VID_Entity = (int)CRC.Calculate("Entity");                   //所属Entity
+        //public static readonly int VID_Skill = (int)CRC.Calculate("Skill");                     //所属Skill
+        //public static readonly int VID_Effect = (int)CRC.Calculate("Effect");                   //所属Effect
+        public static readonly int VID_LevelTable = (int)CRC.Calculate("LevelTable");           //等级数值表
+        public static readonly int VID_Attribute = (int)CRC.Calculate("Attribute");             //属性
+        public static readonly int VID_Value = (int)CRC.Calculate("Value");                     //属性
+        public static readonly int VID_BaseValue = (int)CRC.Calculate("BaseValue");             //属性
+        public static readonly int VID_OriginalSource = (int)CRC.Calculate("OriginalSource");   //当前Effect的原始来源
+        public static readonly int VID_Source = (int)CRC.Calculate("Source");                   //当前技能施放者/当前Effect的直接来源
+        public static readonly int VID_Target = (int)CRC.Calculate("Target");                   //当前技能目标/当前Effect的目标
+        public static readonly int VID_Attacker = (int)CRC.Calculate("Attacker");               //当前攻击来源
+        public static readonly int VID_Defender = (int)CRC.Calculate("Defender");               //当前攻击目标
+        public static readonly int VID_BaseDamage = (int)CRC.Calculate("BaseDamage");           //当前攻击初始伤害
 
         public List<int> m_variable = new List<int>();
 
