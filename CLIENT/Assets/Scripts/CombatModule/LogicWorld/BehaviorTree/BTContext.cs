e554c89313f84411f75e231998d939ada9c0e5dd
cofahoher <cofahoher@163.com>
2017-05-27 11:27:46 +0800

    fix

--- CLIENT/Assets/Scripts/CombatModule/LogicWorld/BehaviorTree/BTContext.cs
+++ CLIENT/Assets/Scripts/CombatModule/LogicWorld/BehaviorTree/BTContext.cs
@@ -2,5 +2,73 @@
 using System.Collections.Generic;
 namespace Combat
 {
-    //TEST
+    public class BTContext : IRecyclable
+    {
+        LogicWorld m_logic_world;
+        Dictionary<int, FixPoint> m_data = new Dictionary<int, FixPoint>();
+        Dictionary<int, object> m_data_ext = new Dictionary<int, object>();
+
+        public void Reset()
+        {
+            m_logic_world = null;
+            m_data.Clear();
+            m_data_ext.Clear();
+        }
+
+        public void SetData(int key, FixPoint value)
+        {
+            m_data[key] = value;
+        }
+
+        public FixPoint GetData(int key)
+        {
+            FixPoint value;
+            if (!m_data.TryGetValue(key, out value))
+                return FixPoint.Zero;
+            return value;
+        }
+
+        public void SetData(string str_key, FixPoint value)
+        {
+            int key = (int)CRC.Calculate(str_key);//key.GetHashCode();
+            m_data[key] = value;
+        }
+
+        public FixPoint GetData(string str_key)
+        {
+            int key = (int)CRC.Calculate(str_key);//key.GetHashCode();
+            FixPoint value;
+            if (!m_data.TryGetValue(key, out value))
+                return FixPoint.Zero;
+            return value;
+        }
+
+        public void SetData<T>(int key, T value)
+        {
+            m_data_ext[key] = value;
+        }
+
+        public T GetData<T>(int key)
+        {
+            object value;
+            if (!m_data_ext.TryGetValue(key, out value))
+                return default(T);
+            return (T)value;
+        }
+
+        public void SetData<T>(string str_key, T value)
+        {
+            int key = (int)CRC.Calculate(str_key);//key.GetHashCode();
+            m_data_ext[key] = value;
+        }
+
+        public T GetData<T>(string str_key)
+        {
+            int key = (int)CRC.Calculate(str_key);//key.GetHashCode();
+            object value;
+            if (!m_data_ext.TryGetValue(key, out value))
+                return default(T);
+            return (T)value;
+        }
+    }
 }
