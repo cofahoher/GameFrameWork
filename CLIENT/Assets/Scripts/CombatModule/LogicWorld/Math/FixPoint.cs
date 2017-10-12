c9c764b850c7ed767dcef7782ea763e228540835
cofahoher <cofahoher@163.com>
2017-10-12 15:48:26 +0800

    fix

--- CLIENT/Assets/Scripts/CombatModule/LogicWorld/Math/FixPoint.cs
+++ CLIENT/Assets/Scripts/CombatModule/LogicWorld/Math/FixPoint.cs
@@ -4,6 +4,7 @@ using BaseUtil;
 
 public partial struct FixPoint : IEquatable<FixPoint>, IComparable<FixPoint>
 {
+
     [ProtoBufAttribute(Index = 1)]
     public long m_raw_value;  //ZZWTODO 为了ProtoBuf，必须public
 
