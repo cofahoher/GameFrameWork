28575ba3e1046c806bb193c44859c343091d4524
cofahoher <cofahoher@163.com>
2017-11-21 16:20:53 +0800

    nth

---
+++ CLIENT/Assets/Scripts/Util/Config/ConfigItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public abstract class ConfigItem
+{
+    public int ID = -1;
+    
+    public virtual bool TryParse(XmlNode node)
+    {
+        return true;
+    }
+}
---
+++ CLIENT/Assets/Scripts/Util/Config/ConfigTable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class ConfigTable
+{
+	private Dictionary<int, ConfigItem> m_ID2ConfigItem = new Dictionary<int, ConfigItem>();
+
+	public ConfigItem GetConfig (int id)
+	{
+        if (m_ID2ConfigItem.ContainsKey(id))
+            return m_ID2ConfigItem[id];
+		else
+			return null;
+	}
+}
+
+
